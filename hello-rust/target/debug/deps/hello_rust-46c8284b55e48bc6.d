dbae809372c28922fc054e54d21fab7828a5940c
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-07-04 17:24:11 -0300

    hello world.

---
+++ hello-rust/target/debug/deps/hello_rust-46c8284b55e48bc6.d
@@ -0,0 +1,5 @@
+/home/s2it_faquino/Documents/Personal/workspace/rust-playground/hello-rust/target/debug/deps/hello_rust-46c8284b55e48bc6: src/main.rs
+
+/home/s2it_faquino/Documents/Personal/workspace/rust-playground/hello-rust/target/debug/deps/hello_rust-46c8284b55e48bc6.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/s2it_faquino/Documents/Personal/workspace/rust-playground/hello-rust/target/debug/hello-rust: /home/s2it_faquino/Documents/Personal/workspace/rust-playground/hello-rust/src/main.rs
